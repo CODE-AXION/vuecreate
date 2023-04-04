61964ebea2730725e10887487370266764693334
codeaxion77@gmail.co <CODE-AXION>
2023-04-04 15:03:59 +0530

    added

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+
+<HeaderMenu title="Hello world" />
+
+</template>
+
+<script>
+
+import HeaderMenu from './components/Header.vue';
+
+export default {
+  
+  name: 'App',
+
+  components: {
+    HeaderMenu,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <h1>Tast Tracker {{title}}</h1>
+    </header>
+</template>
+
+<script>
+
+export default {
+    name: 'HeaderMenu',
+    props: {
+        title:{
+            type: String,
+            default: ' Hello world'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+    HeaderMenu{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
